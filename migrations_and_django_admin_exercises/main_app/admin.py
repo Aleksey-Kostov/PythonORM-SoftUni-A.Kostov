b799a5a3c8c9322d56f2a922ff59ebd358eaea13
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-28 17:02:59 +0300

    Tasks from course PythonORM

--- migrations_and_django_admin_exercises/main_app/admin.py
+++ migrations_and_django_admin_exercises/main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from main_app.models import EventRegistration, Movie, Student, Supplier
+from main_app.models import EventRegistration, Movie, Student, Supplier, Course
 
 
 @admin.register(EventRegistration)
@@ -47,3 +47,19 @@ class SupplierAdmin(admin.ModelAdmin):
         }),
     )
 
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lecturer', 'price', 'start_date')
+    list_filter = ('is_published', 'lecturer')
+    search_fields = ('lecturer', 'title')
+    readonly_fields = ('start_date', )
+
+    fieldsets = (
+        ('Course Information', {
+            'fields': ('title', 'lecturer', 'price', 'start_date', 'is_published'),
+        }),
+        ('Description', {
+            'fields': ('description', ),
+        })
+    )
