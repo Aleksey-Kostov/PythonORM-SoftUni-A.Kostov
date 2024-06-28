ec54f03ca9198d34ab19e2e870e6b2e4d8cf0740
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-28 17:31:35 +0300

    Tasks from course PythonORM

--- migrations_and_django_admin_exercises/main_app/models.py
+++ migrations_and_django_admin_exercises/main_app/models.py
@@ -159,3 +159,21 @@ class Item(models.Model):
         max_length=20,
         default="No rarity",
     )
+
+
+class SmartPhone(models.Model):
+    brand = models.CharField(
+        max_length=100,
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+
+    category = models.CharField(
+        max_length=20,
+        default="No category",
+    )
+
