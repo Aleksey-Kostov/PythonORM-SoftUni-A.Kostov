3c345c6e96498511393057133c82365d157a13a5
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-30 15:11:25 +0300

    Tasks from course PythonORM

--- data_operations_in_django_with_queries_exercises/caller.py
+++ data_operations_in_django_with_queries_exercises/caller.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Pet, Artifact, Location, Car
+from main_app.models import Pet, Artifact, Location, Car, Task
 from django.db.models import QuerySet
 
 
@@ -76,3 +76,25 @@ def get_recent_cars() -> QuerySet:
 
 def delete_last_car() -> None:
     Car.objects.last().delete()
+
+
+def show_unfinished_tasks() -> str:
+    unfinished_tasks = Task.objects.filter(is_finished=False)
+
+    return "\n".join(str(t) for t in unfinished_tasks)
+
+
+def complete_odd_tasks() -> None:
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        if task.id % 2 == 1:
+            task.is_finished = True
+
+    Task.objects.bulk_update(tasks, ['is_finished'])
+
+
+def encode_and_replace(text: str, task_title: str) -> None:
+    decoded_text = ''.join(chr(ord(symbol) - 3) for symbol in text)
+    Task.objects.filter(title=task_title).update(description=decoded_text)
+
--- data_operations_in_django_with_queries_exercises/main_app/models.py
+++ data_operations_in_django_with_queries_exercises/main_app/models.py
@@ -26,12 +26,22 @@ class Location(models.Model):
 
 
 class Car(models.Model):
-    model = models.CharField(max_length=40,)
+    model = models.CharField(max_length=40, )
     year = models.PositiveIntegerField()
-    color = models.CharField(max_length=40,)
-    price = models.DecimalField(max_digits=10, decimal_places=2,)
+    color = models.CharField(max_length=40, )
+    price = models.DecimalField(max_digits=10, decimal_places=2, )
     price_with_discount = models.DecimalField(
         max_digits=10,
         decimal_places=2,
         default=0
     )
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=25,)
+    description = models.TextField()
+    due_date = models.DateField()
+    is_finished = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Task - {self.title} needs to be done until {self.due_date}!"
