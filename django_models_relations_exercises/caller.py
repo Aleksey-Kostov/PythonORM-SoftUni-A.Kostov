1d1a8342610f8b204b41b173cde79e54b410aadb
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-07 12:08:18 +0300

    Tasks from course PythonORM

--- django_models_relations_exercises/caller.py
+++ django_models_relations_exercises/caller.py
@@ -5,6 +5,28 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Author, Book
 
-# Create queries within functions
+
+def show_all_authors_with_their_books() -> str:
+    authors_with_books = []
+
+    authors = Author.objects.all().order_by("id")  # select prefetch prefetch_related
+
+    for author in authors:
+        books = Book.objects.filter(author=author)
+        # books = author.book_set.all() option 2
+
+        if not books:
+            continue
+
+        titles = ", ".join(b.title for b in books)
+        authors_with_books.append(
+            f"{author.name} has written - {titles}!"
+        )
+
+    return "\n".join(authors_with_books)
+
+
+def delete_all_authors_without_books() -> None:
+    Author.objects.filter(book__isnull=True).delete()
