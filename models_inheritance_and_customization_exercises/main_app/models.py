645a317a67bfa75f7d7703de49e432043ab7db93
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-11 20:50:19 +0300

    Tasks from course PythonORM

--- models_inheritance_and_customization_exercises/main_app/models.py
+++ models_inheritance_and_customization_exercises/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseCharacter(models.Model):
+    class Meta:
+        abstract = True
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Mage(BaseCharacter):
+    elemental_power = models.CharField(max_length=100)
+    spellbook = models.CharField(max_length=100)
