bfaad658da6247d96b3b61608a9ca46fc54b70d3
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-14 18:12:08 +0300

    Tasks from course PythonORM

--- models_inheritance_and_customization_exercises/main_app/models.py
+++ models_inheritance_and_customization_exercises/main_app/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -75,4 +76,94 @@ class FelbladeDemonHunter(DemonHunter):
     felblade_ability = models.CharField(
         max_length=100,
     )
+
+
+class UserProfile(models.Model):
+    username = models.CharField(
+        max_length=70,
+        unique=True,
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    bio = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        to=UserProfile,
+        on_delete=models.CASCADE,
+        related_name="sent_messages",
+    )
+
+    receiver = models.ForeignKey(
+        to=UserProfile,
+        on_delete=models.CASCADE,
+        related_name="received_messages",
+    )
+
+    content = models.TextField()
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    is_read = models.BooleanField(
+        default=False,
+    )
+
+    def mark_as_read(self) -> None:
+        self.is_read = True
+        # good to save it
+
+    def reply_to_message(self, reply_content: str) -> "Message":
+        message = Message(
+            sender=self.receiver,
+            receiver=self.sender,
+            content=reply_content,
+        )
+
+        message.save()
+
+        return message
+
+    def forward_message(self, receiver: UserProfile) -> "Message":
+        message = Message(
+            sender=self.receiver,
+            receiver=receiver,
+            content=self.content,
+        )
+
+        message.save()
+
+        return message
+
+
+class StudentIDField(models.PositiveIntegerField):
+    def to_python(self, value) -> int or None:
+        try:
+            return int(value)  # int("dido")
+        except ValueError:
+            raise ValueError("Invalid input for student ID")
+
+    def get_prep_value(self, value):
+        cleaned_value = self.to_python(value)
+
+        if cleaned_value <= 0:
+            raise ValidationError("ID cannot be less than or equal to zero")
+
+        return cleaned_value
+
+
+class Student(models.Model):
+    name = models.CharField(
+        max_length=100,
+    )
+
+    student_id = StudentIDField()
 
