ffbfb90dbb21500615abf0391e043ae0e1e86734
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-15 22:37:12 +0300

    Tasks from course PythonORM

--- advanced_django_model_techniques_exercises/caller.py
+++ advanced_django_model_techniques_exercises/caller.py
@@ -1,10 +1,18 @@
 import os
+from decimal import Decimal
+
 import django
+
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Document
 
-# Create queries within functions
+from django.contrib.postgres.search import SearchVector
+
+results = Document.objects.filter(search_vector='django web framework')
+
+for result in results:
+    print(f"Title: {result.title}")
