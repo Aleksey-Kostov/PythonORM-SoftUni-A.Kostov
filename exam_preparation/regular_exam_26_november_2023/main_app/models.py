95f9ee79449b5e23c3b4d80471c76306510dbded
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-22 20:33:17 +0300

    Tasks from course PythonORM

--- exam_preparation/regular_exam_26_november_2023/main_app/models.py
+++ exam_preparation/regular_exam_26_november_2023/main_app/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    email = models.EmailField(unique=True)
+    is_banned = models.BooleanField(default=False)
+    birth_year = models.PositiveIntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2005)])
+    website = models.URLField(Null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Article(models.Model):
+    class ArticleCategory(models.TextChoices):
+        TECHNOLOGY = 'Technology', 'Technology'
+        SCIENCE = 'Science', 'Science'
+        EDUCATION = 'Education', 'Education'
+
+    title = models.CharField(max_length=200, validators=MinLengthValidator(5))
+    content = models.TextField(validators=MinLengthValidator(10))
+    category = models.CharField(choices=ArticleCategory, default='Technology', max_length=10)
+    authors = models.ManyToManyField(Author, related_name='article')
+    published_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    content = models.TextField(validators=MinLengthValidator(10))
+    rating = models.FloatField(validators=MinValueValidator(1.0), max_length=5.0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='reviews')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='reviews')
+    published_on = models.DateTimeField(auto_now_add=True, editable=False)
