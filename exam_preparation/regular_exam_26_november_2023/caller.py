4c3e460f46f369c40cacf3263aa125fce085b1a7
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-22 23:06:25 +0300

    Tasks from course PythonORM

--- exam_preparation/regular_exam_26_november_2023/caller.py
+++ exam_preparation/regular_exam_26_november_2023/caller.py
@@ -5,6 +5,42 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Author, Review
+from django.db.models import Count
 
-# Create queries within functions
+
+def get_authors(search_name=None, search_email=None):
+    if search_name is None and search_email is None:
+        return ''
+
+    if search_name is not None and search_email is not None:
+        authors = (Author.objects.filter
+                   (full_name__icontains=search_name, email__icontains=search_email).order_by('-full_name'))
+
+    elif search_name is not None:
+        authors = Author.objects.fiilter(full_name__icontains=search_name).order_by('-full_name')
+
+    else:
+        authors = Author.objects.fiilter(email__icontains=search_email).order_by('-full_name')
+
+    result = []
+
+    [result.append(f"Author: {a.full_name}, email: {a.mail}, status: {'Banned' if a.is_banned else 'Not Banned'}")
+     for a in authors if authors]
+
+    return "\n".join(result) if result else ""
+
+
+def get_top_publisher():
+    author = Author.objects.get_authors_by_article_count.first()
+    if author is None or author.article_count == 0:
+        return ""
+
+    return f"Top Author: {author.full_name} with {author.article_count} published articles."
+
+
+def get_top_reviewer():
+    author_review = Author.objects.annotate(num_review=Count('reviews')).order_by('-num_review', 'email').first()
+    if author_review is None or author_review.num_review == 0:
+        return ''
+    return f"Top Reviewer: {author_review.full_name} with {author_review.num_review} published reviews."
