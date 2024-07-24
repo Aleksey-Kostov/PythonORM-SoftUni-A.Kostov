b3c0f50654e5c4b17991268aec43bedf9dddcae0
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-24 22:35:16 +0300

    Tasks from course PythonORM

--- exam_preparation/regular_exam_26_november_2023/caller.py
+++ exam_preparation/regular_exam_26_november_2023/caller.py
@@ -5,8 +5,8 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-from main_app.models import Author
-from django.db.models import Count
+from main_app.models import Author, Article
+from django.db.models import Count, Avg
 
 
 def get_authors(search_name=None, search_email=None):
@@ -43,4 +43,49 @@ def get_top_reviewer():
     author_review = Author.objects.annotate(num_review=Count('reviews')).order_by('-num_review', 'email').first()
     if author_review is None or author_review.num_review == 0:
         return ''
+
     return f"Top Reviewer: {author_review.full_name} with {author_review.num_review} published reviews."
+
+
+def get_latest_article():
+    latest_article = Article.objects.prefetch_related('authors', 'reviews').order_by('-published_on').first()
+
+    if latest_article is None:
+        return ''
+
+    author_name = ', '.join(a.full_name for a in latest_article.authors.all().order_by('full_name'))
+    num_reviews = latest_article.reviews.count()
+    avg_reviews_rating = sum([r.rating for r in latest_article.reviews.all()]) / num_reviews if num_reviews else 0.0
+
+    return f"The latest article is: {latest_article.title}. Authors: {author_name}. Reviewed: {num_reviews} times." \
+           f" Average Rating: {avg_reviews_rating:.2f}."
+
+
+def get_top_rated_article():
+    top_article = Article.objects.annotate(avg_rating=Avg('reviews__rating')).order_by('-avg_rating', 'title').first()
+
+    num_reviews = top_article.reviews.count() if top_article else 0
+
+    if top_article is None or num_reviews == 0:
+        return ''
+
+    avg_rating = top_article.avg_rating or 0.0
+
+    return (f"The top-rated article is: {top_article.title}, "
+            f"with an average rating of {avg_rating:.2f}, "
+            f"reviewed {num_reviews} times.")
+
+
+def ban_author(email=None):
+    author = Author.objects.prefetch_related('reviews').filter(email__exact=email).first()
+
+    if author is None or email is None:
+        return 'No authors banned.'
+
+    number_reviews = author.reviews.count()
+
+    author.is_banned = True
+    author.save()
+    author.reviews.all().delete()
+
+    return f"Author: {author.full_name} is banned! {number_reviews} reviews deleted."
