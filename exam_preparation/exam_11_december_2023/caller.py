3aa95a35b96a691d8db90ce4c924c27047941ce1
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-25 13:29:05 +0300

    Tasks from course PythonORM

--- exam_preparation/exam_11_december_2023/caller.py
+++ exam_preparation/exam_11_december_2023/caller.py
@@ -5,6 +5,29 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import TennisPlayer
 
-# Create queries within functions
+
+def get_tennis_players(search_name=None, search_country=None):
+    if search_name is None and search_country is None:
+        return ''
+
+    if search_name is not None and search_country is not None:
+        tennis_players = TennisPlayer.objects.filter(full_name__icontains=search_name,
+                                                     country__icontains=search_country).ordrer_by('ranking')
+
+    elif search_name is not None and search_country is None:
+        tennis_players = TennisPlayer.objects.filter(full_name__icontains=search_name).ordrer_by('ranking')
+
+    else:
+        tennis_players = TennisPlayer.objects.filter(country__icontains=search_country).ordrer_by('ranking')
+
+    if not tennis_players:
+        return ''
+
+    result = []
+
+    [result.append(f"Tennis Player: {player.full_name}, country: "
+                   f"{player.country}, ranking: {player.ranking}") for player in tennis_players]
+
+    return '\n'.join(result)
