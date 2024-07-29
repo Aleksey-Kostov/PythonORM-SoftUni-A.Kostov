3dc622664095341b869c26b7fb773b92f6e7d84d
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-29 16:54:43 +0300

    Tasks from course PythonORM

--- exam_preparation/exam_prep_2/caller.py
+++ exam_preparation/exam_prep_2/caller.py
@@ -6,7 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 from main_app.models import Profile, Product, Order
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 
 
 def get_profiles(search_string=None):
@@ -39,12 +39,58 @@ def get_loyal_profiles():
 
 
 def get_last_sold_products():
-    latest_order = Order.objects.prefetch_related('products').latest('creation_date')
-    order_products = latest_order.products.all().order_by('name')
+    try:
+        latest_order = Order.objects.prefetch_related('products').latest('creation_date')
+        order_products = latest_order.products.all().order_by('name')
+        if not order_products:
+            return ''
+        all_products = ', '.join(p.name for p in order_products)
 
-    if not latest_order or not order_products:
+        return f"Last sold products: {all_products}"
+    except Order.DoesNotExist:
         return ''
 
-    all_products = ', '.join(p.name for p in order_products)
 
-    return f"Last sold products: {all_products}"
+def get_top_products():
+    top_products = (Product.objects.prefetch_related('product_orders').annotate(num_orders=Count('product_orders'))
+                    .filter(num_orders__gt=0)
+                    .order_by('-num_orders', 'name'))[:5]
+
+    if top_products:
+        # result = '\n'.join(f"{p.name}, sold {p.num_orders} times" for p in top_products)
+        result = []
+        [result.append(f"{p.name}, sold {p.num_orders} times") for p in top_products]
+        result = '\n'.join(result)
+        return f"Top products:\n{result}"
+    return ''
+
+
+def apply_discounts():
+    orders = ((Order.objects.annotate(num_of_updated_orders=Count('products')))
+              .filter(num_of_updated_orders__gt=2, is_completed=False)
+              .update(total_price=F('total_price') * 0.9))
+
+    return f"Discount applied to {orders} orders."
+
+
+def complete_order():
+    com_order = (Order.objects.prefetch_related('products')
+                 .filter(is_completed=False)
+                 .order_by('creation_date')
+                 .first())
+
+    if com_order is None:
+        return ''
+
+    com_order.is_completed = True
+    com_order.save()
+
+    for product in com_order.products.all():
+        product.in_stock -= 1
+
+        if product.in_stock == 0:
+            product.is_available = False
+
+        product.save()
+
+    return 'Order has been completed!'
