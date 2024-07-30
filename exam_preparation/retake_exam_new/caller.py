c9e90ff0066109f588e72e8cc0b9706c009bcdbe
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-31 00:46:31 +0300

    Tasks from course PythonORM

--- exam_preparation/retake_exam_new/caller.py
+++ exam_preparation/retake_exam_new/caller.py
@@ -5,6 +5,93 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import TennisPlayer, Tournament, Match
+from django.db.models import Count
 
-# Create queries within functions
+
+def get_tennis_players(search_name=None, search_country=None):
+    if search_name is None and search_country is None:
+        return ''
+
+    if search_name is not None and search_country is not None:
+        tennis_player = (
+            TennisPlayer.objects.filter(country__icontains=search_country, full_name__icontains=search_name)
+            .order_by('ranking'))
+
+    elif search_name is not None:
+        tennis_player = TennisPlayer.objects.filter(full_name__icontains=search_name).order_by('ranking')
+
+    else:
+        tennis_player = TennisPlayer.objects.filter(country__icontains=search_country).order_by('ranking')
+
+    if tennis_player is None:
+        return ''
+
+    result = []
+
+    [result.append(f"Tennis Player: {tp.full_name}, country: {tp.country}, "
+                   f"ranking: {tp.ranking}") for tp in tennis_player]
+
+    return '\n'.join(result)
+
+
+def get_top_tennis_player():
+    tennis_player = (TennisPlayer.objects.annotate(num_wins=Count('winner'))
+                     .order_by('-num_wins', 'full_name')
+                     .first())
+
+    if tennis_player is None:
+        return ''
+
+    return f'Top Tennis Player: {tennis_player.full_name} with {tennis_player.num_wins} wins.'
+
+
+def get_tennis_player_by_matches_count():
+    tennis_player = (TennisPlayer.objects.annotate(matches_count=Count('players_matches'))
+                     .order_by('-matches_count', 'ranking')
+                     .first())
+
+    if tennis_player is None or tennis_player.matches_count == 0:
+        return ''
+
+    return f"Tennis Player: {tennis_player.full_name} with {tennis_player.matches_count} matches played."
+
+
+def get_tournaments_by_surface_type(surface=None):
+    if surface is None:
+        return ''
+    tournaments = (Tournament.objects.prefetch_related('tournament_matches')
+                   .annotate(num_matches=Count('tournament_matches'))
+                   .filter(surface_type__icontains=surface).order_by('-start_date'))
+
+    if tournaments is None:
+        return ''
+
+    result = []
+    [result.append(f"Tournament: {t.name}, start date: {t.start_date}, "
+                   f"matches: {t.num_matches}") for t in tournaments]
+
+    return "\n".join(result)
+
+
+def get_latest_match_info():
+    latest_match = (Match.objects.select_related('tournament').prefetch_related('players_matches')
+                    .order_by('-date_played', '-id').first())
+
+    if latest_match is None:
+        return ''
+
+    players = latest_match.players.order_by('full_name')
+    player_1 = players.first().full_name
+    player_2 = players.last().full_name
+    players_full_name = f"{player_1} vs {player_2}"
+
+    return (f"Latest match played on: {latest_match.date_played}, "
+            f"tournament: {latest_match.tournament.name}, "
+            f"score: {latest_match.score}, players: {players_full_name}, "
+            f"winner: {'TBA' if latest_match.winner is None else latest_match.winner.full_name}, "
+            f"summary: {latest_match.summary}")
+
+
+def get_matches_by_tournament(tournament_name=None):
+    pass
