aae59c2a1b1103855fbc35607caa2f41f08400fc
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-30 21:29:26 +0300

    Tasks from course PythonORM

--- exam_preparation/retake_exam_new/main_app/admin.py
+++ exam_preparation/retake_exam_new/main_app/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
+from django.db import models
 
-# Register your models here.
+
