feaa2de8cdcdf14be95279e7891e2ab7a4b2f4e8
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-31 16:49:14 +0300

    Tasks from course PythonORM

--- exam_preparation/exam_prep_1_new/caller.py
+++ exam_preparation/exam_prep_1_new/caller.py
@@ -5,6 +5,61 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
-# Import your models here
+from main_app.models import Director, Actor
+from django.db.models import Count, Avg
 
-# Create queries within functions
+
+def get_directors(search_name=None, search_nationality=None):
+    if search_name is None and search_nationality is None:
+        return ''
+
+    if search_name is not None and search_nationality is not None:
+        directors = (Director.objects
+                     .filter(full_name__icontains=search_name, nationality__icontains=search_nationality)
+                     .order_by('full_name'))
+
+    elif search_name:
+        directors = (Director.objects
+                     .filter(full_name__icontains=search_name)
+                     .order_by('full_name'))
+
+    else:
+        directors = (Director.objects
+                     .filter(nationality__icontains=search_nationality)
+                     .order_by('full_name'))
+
+    if directors is None:
+        return ''
+
+    result = []
+
+    [result.append(f"Director: {d.full_name}, "
+                   f"nationality: {d.nationality}, "
+                   f"experience: {d.years_of_experience}") for d in directors]
+
+    return '\n'.join(result)
+
+
+def get_top_director():
+    director = Director.objects.get_directors_by_movies_count()
+
+    if director is None:
+        return ''
+
+    return f"Top Director: {director.full_name}, movies: {director.num_movies}."
+
+
+def get_top_actor():
+    actors = (Actor.objects.prefetch_related('starring_actor_movies')
+              .annotate(num_starring=Count('starring_actor_movies'),
+                        movies_avg_rating=Avg('starring_actor_movies__rating'))
+              .order_by('-num_starring', 'full_name')
+              .first())
+    if not actors or not actors.num_starring:
+        return ''
+
+    movies_title = ', '.join(a.title for a in actors.starring_actor_movies.all() if a)
+
+    return (f"Top Actor: {actors.full_name}, "
+            f"starring in movies: {movies_title}, "
+            f"movies average rating: {actors.movies_avg_rating:.1f}")
