953d62f3f195d227532f904a274f53e3a9b8bcf9
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-07-31 15:27:59 +0300

    Tasks from course PythonORM

--- exam_preparation/exam_prep_1_new/main_app/models.py
+++ exam_preparation/exam_prep_1_new/main_app/models.py
@@ -1,3 +1,39 @@
+from django.core.validators import MinLengthValidator, MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    years_of_experience = models.SmallIntegerField(validators=[MinValueValidator(0)], default=0)
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=120, validators=[MinLengthValidator(2)])
+    birth_date = models.DateField(default='1900-01-01')
+    nationality = models.CharField(max_length=50, default='Unknown')
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now_add=True)
+
+
+class Movie(models.Model):
+    class GenreChoices(models.TextChoices):
+        ACTION = 'Action', 'Action'
+        COMEDY = 'Comedy', 'Comedy'
+        DRAMA = 'Drama', 'Drama'
+        OTHER = 'Other', 'Other'
+    title = models.CharField(max_length=150, validators=[MinLengthValidator(5)])
+    release_date = models.DateField()
+    storyline = models.TextField(null=True, blank=True)
+    genre = models.CharField(max_length=6, choices=GenreChoices, default='Other')
+    rating = models.DecimalField(max_digits=3, decimal_places=1,
+                                 validators=[MinValueValidator(0.0), MaxValueValidator(10)],
+                                 default=0.0)
+    is_classic = models.BooleanField(default=False)
+    is_awarded = models.BooleanField(default=False)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, related_name='director_movies')
+    starring_actor = models.ForeignKey(Actor, on_delete=models.SET_NULL, null=True, blank=True,
+                                       related_name='starring_actor_movies')
+    actors = models.ManyToManyField(Actor, related_name='actors_movies')
